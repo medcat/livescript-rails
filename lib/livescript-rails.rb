dc33596232e7ea232262ac14e97ae6e2bdd043b6
Jeremy Rodi <me@medcat.me>
2016-08-30 21:40:04 -0600

    Remove Hard Rails Dependency
    Instead of always defining a Railtie, define it if `::Rails` exists, and provide the `register!` function.

--- lib/livescript-rails.rb
+++ lib/livescript-rails.rb
@@ -1,4 +1,13 @@
 require 'livescript'
 require 'livescript/rails/version'
 require 'livescript/rails/processor'
-require 'livescript/rails/railtie'
+require 'livescript/rails/railtie' if defined?(::Rails)
+
+module Livescript
+  module Rails
+    def self.register!
+      Sprockets.register_mime_type 'text/livescript', extensions: ['.ls', '.js.ls']
+      Sprockets.register_transformer 'text/livescript', 'application/javascript', LiveScript::Rails::SprocketsProcessor
+    end
+  end
+end
